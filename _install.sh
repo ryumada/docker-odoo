6c831e9a9d88ee2dedb78c9c0ef48f9579536c34
ryumada <rizukiryumada@tilabs.id>
2024-10-21 13:35:28 +0700

    add setPermissionFileToReadOnlyAndOnlyTo function when the user already exists

--- _install.sh
+++ _install.sh
@@ -97,6 +97,7 @@ function generatePostgresSecrets() {
 
   if sudo -u postgres psql -tAc "SELECT 1 FROM pg_roles WHERE rolname='$POSTGRES_ODOO_USERNAME'" 2>/dev/null | grep -q 1; then
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] ✅ User $POSTGRES_ODOO_USERNAME already exists."
+    setPermissionFileToReadOnlyAndOnlyTo "$ODOO_LINUX_USER" "$DB_USER_SECRET"
   else
     echo "[$(date +"%Y-%m-%d %H:%M:%S")] 🟦 User $POSTGRES_ODOO_USERNAME doesn't exist. Creating the user..."
     sudo -u postgres psql -c "CREATE ROLE \"$POSTGRES_ODOO_USERNAME\" LOGIN CREATEDB;" > /dev/null 2>&1
