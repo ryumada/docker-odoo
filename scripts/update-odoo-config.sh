477950581af86cfc4959743049ad02563310ce45
ryumada <rizukiryumada@tilabs.id>
2025-12-03 14:05:06 +0700

    add support to add custom odoo configuration via .env file

--- scripts/update-odoo-config.sh
+++ scripts/update-odoo-config.sh
@@ -53,24 +53,35 @@ if [ -f "$ENV_FILE" ]; then
     [[ -z "$line" || "$line" =~ ^# ]] && continue
     # shellcheck disable=SC2163
     export "$line"
-  done < "$ENV_FILE"
+  done < <(tr -d '\r' < "$ENV_FILE")
 
   # Check if the ADMIN_PASSWD variable is set in the .env file
   if [ -n "$ADMIN_PASSWD" ]; then
+    log_info "Injecting admin_passwd to $ODOO_CONF..."
     # Replace the admin_passwd value in the odoo.conf file
     sed -i "s|enter_random_password_string|$ADMIN_PASSWD|" "$ODOO_CONF"
-    log_success "Updated admin_passwd in odoo.conf."
+    log_success "Updated admin_passwd in $ODOO_CONF."
   else
     log_warn "ADMIN_PASSWD not set in .env file. Using default value from odoo.conf.example."
   fi
 
   # Check if the ADDONS_PATH variable is set in the .env file
   if [ -n "$ADDONS_PATH" ]; then
+    log_info "Injecting addons_path to $ODOO_CONF..."
     # Replace the addons_path value in the odoo.conf file
     sed -i "s|enter_addons_paths|$ADDONS_PATH|" "$ODOO_CONF"
-    log_success "Updated addons_path in odoo.conf."
+    log_success "Updated addons_path in $ODOO_CONF."
   else
     log_warn "ADDONS_PATH not set in .env file. Using default value from odoo.conf.example."
+  fi
+
+  if [ -n "$ODOO_ADDITIONAL_CONF" ]; then
+    log_info "Injecting Additional odoo.conf configuration to $ODOO_CONF..."
+    FORMATTED_CONF="\n# Additional custom odoo configuration generated from .env file\n$(echo "$ODOO_ADDITIONAL_CONF" | sed 's/=/ = /g; s/\;/\\n/g')"
+    sed -i "/^proxy_mode = True/a $FORMATTED_CONF" "$ODOO_CONF"
+    log_success "Appended ODOO_ADDITIONAL_CONF to $ODOO_CONF."
+  else
+    log_warn "ODOO_ADDITIONAL_CONF not set in .env file. Additional odoo.conf will not be added."
   fi
 else
   log_warn ".env file not found. Using default values from odoo.conf.example."
